ce6dd0285140bcfccff567cbf69ff9e6795041f1
sandeep <sandeep@DESKTOP-78NR8IK.attlocal.net>
2021-08-03 23:09:37 -0400

    Swap

---
+++ LearnJava/src/com/programs/LearnSwapping.java
@@ -0,0 +1,22 @@
+package com.programs;
+
+import java.util.Scanner;
+
+public class LearnSwapping {
+
+	public static void main(String[] args) {
+
+		 int x=77;
+		 int y=44;
+		 
+
+	      x = x + y;
+	      y = x - y;
+	      x = x - y;
+
+	      System.out.println("Values After Swapping,");
+	      System.out.println("x="+x+"\t y="+y);
+	   }
+	}
+
+
